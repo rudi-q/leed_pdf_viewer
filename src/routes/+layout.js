3aca7200126b1b1cf77fb5e5615b0799a5fcdba7
Rudi K. <rudi@doubl.one>
2025-08-04 23:15:35 +0800

    Added Vercel Analytics

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,2 +1,7 @@
+import { dev } from '$app/environment';
+import { injectAnalytics } from '@vercel/analytics/sveltekit';
+
+injectAnalytics({ mode: dev ? 'development' : 'production' });
+
 export const prerender = true;
-export const ssr = false; // Disable SSR for full client-side app
+export const ssr = false; // Disable SSR for the full client-side app
