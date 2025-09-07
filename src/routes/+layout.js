29ef04a6a7be67879a1e55289b7a64c2bf477829
Rudi K. <rudi@doubl.one>
2025-09-07 19:38:20 +0800

    initial implement of anonymized product insights

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,7 +1,24 @@
 import { dev } from '$app/environment';
 import { injectAnalytics } from '@vercel/analytics/sveltekit';
+import posthog from 'posthog-js'
+import { browser } from '$app/environment';
+import { PUBLIC_POSTHOG_KEY } from '$env/static/public';
 
 injectAnalytics({ mode: dev ? 'development' : 'production' });
 
 export const prerender = true;
 export const ssr = false; // Disable SSR for the full client-side app
+
+export const load = async () => {
+	if (browser && !dev && PUBLIC_POSTHOG_KEY) {
+		posthog.init(
+			PUBLIC_POSTHOG_KEY,
+			{
+				api_host: 'https://eu.i.posthog.com',
+				defaults: '2025-05-24',
+				cookieless_mode: 'always',
+				person_profiles: 'always'
+			}
+		)
+	}
+};
