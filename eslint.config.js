f75e3855b03d91973db6da362053e2ea6f9ca259
Zoé Gilbert <zoegilbert.design@gmail.com>
2026-03-29 16:54:59 +0800

    chore: remove globals package

--- eslint.config.js
+++ eslint.config.js
@@ -1,32 +1,26 @@
 import js from '@eslint/js';
-import tsPlugin from '@typescript-eslint/eslint-plugin';
+import { configs } from '@typescript-eslint/eslint-plugin';
 import tsParser from '@typescript-eslint/parser';
 import sveltePlugin from 'eslint-plugin-svelte';
 import svelteParser from 'svelte-eslint-parser';
-import globals from 'globals';
 
 export default [
   js.configs.recommended,
+  ...configs.recommended,
   ...sveltePlugin.configs['flat/recommended'],
   {
-    files: ['**/*.ts', '**/*.tsx', '**/*.svelte', '**/*.js'],
-    plugins: {
-      '@typescript-eslint': tsPlugin
-    },
     languageOptions: {
       parser: tsParser,
       ecmaVersion: 2020,
       sourceType: 'module',
       globals: {
-        ...globals.browser,
-        ...globals.node,
-        ...globals.es2017
+        browser: true,
+        es2017: true,
+        node: true
       }
     },
     rules: {
-      ...tsPlugin.configs.recommended.rules,
-      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-      '@typescript-eslint/no-explicit-any': 'off'
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }]
     }
   },
   {
@@ -42,11 +36,8 @@ export default [
     ignores: [
       'build/',
       '.svelte-kit/',
-      '.vercel/',
       'dist/',
-      'node_modules/',
-      'src-tauri/target/',
-      'static/'
+      'node_modules/'
     ]
   }
 ];
